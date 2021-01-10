340347ab5f95d7788f9ec822f315d15b759d5167
Giang Nguyen <gn02.nguyen@gmail.com>
2021-01-10 15:44:24 -0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Giang Nguyenn on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
