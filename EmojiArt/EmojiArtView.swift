e4e1c37fc8fcb38a46feb365df36fa56cd7a48fa
Giang Nguyen <gn02.nguyen@gmail.com>
2021-01-16 17:46:40 -0800

    Smoother pan gesture.

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -10,44 +10,46 @@ import SwiftUI
 struct EmojiArtView: View {
     @ObservedObject var  document: EmojiArtDocument
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(EmojiArtDocument.palette.map { String($0)}, id: \.self) { emoji in
-                    Text(emoji).font(Font.system(size: defaultEmojiSize))
-                        .onDrag {
-                            return NSItemProvider(object: emoji as NSString)
-                        }
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map { String($0)}, id: \.self) { emoji in
+                        Text(emoji).font(Font.system(size: defaultEmojiSize))
+                            .onDrag {
+                                return NSItemProvider(object: emoji as NSString)
+                            }
+                    }
                 }
+            }.padding()
+            GeometryReader { geometry in
+                ZStack {
+                    Rectangle().foregroundColor(.white).overlay(
+                        OptionalImage(image: document.backgroundImage)
+                            .scaleEffect(zoomScale)
+                            .offset(panOffset)
+                    )
+                    .gesture(doubleTapToZoom(in: geometry.size))
+                    ForEach(self.document.emojis) { emoji in
+                        Text(emoji.text)
+                            .font(animatableWithSize: zoomScale * defaultEmojiSize)
+                            .position(self.position(for: emoji, in: geometry.size))
+                    }
+                }
+                .clipped()
+                .ignoresSafeArea(edges: [/*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/, .horizontal])
+                .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
+                    var location = geometry.convert(location, from: .global)
+                    location = CGPoint(x: location.x - geometry.size.width / 2,
+                                       y: location.y - geometry.size.height / 2)
+                    location = CGPoint(x: location.x - panOffset.width,
+                                       y: location.y - panOffset.height)
+                    location = location / zoomScale
+                    return self.drop(providers: providers, at: location)
+                }
+                .gesture(panGesture())
+                .gesture(zoomGesture())
             }
-        }.padding()
 
-        GeometryReader { geometry in
-            ZStack {
-                Rectangle().foregroundColor(.white).overlay(
-                    OptionalImage(image: document.backgroundImage)
-                        .scaleEffect(zoomScale)
-                        .offset(panOffset)
-                )
-                .gesture(doubleTapToZoom(in: geometry.size))
-                ForEach(self.document.emojis) { emoji in
-                    Text(emoji.text)
-                        .font(animatableWithSize: zoomScale * defaultEmojiSize)
-                        .position(self.position(for: emoji, in: geometry.size))
-                }
-            }
-            .clipped()
-            .ignoresSafeArea(edges: [/*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/, .horizontal])
-            .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
-                var location = geometry.convert(location, from: .global)
-                location = CGPoint(x: location.x - geometry.size.width / 2,
-                                   y: location.y - geometry.size.height / 2)
-                location = CGPoint(x: location.x - panOffset.width,
-                                   y: location.y - panOffset.height)
-                location = location / zoomScale
-                return self.drop(providers: providers, at: location)
-            }
-            .gesture(zoomGesture())
-            .gesture(panGesture())
         }
     }
     
@@ -76,7 +78,7 @@ struct EmojiArtView: View {
         MagnificationGesture()
             .onEnded { finalGestureScale in
                 steadyStateZoomScale *= finalGestureScale
-            }.updating($gestureZoomScale) { latestGestureScale, gestureZoomScale, transition in
+            }.updating($gestureZoomScale) { latestGestureScale, gestureZoomScale, transaction in
                 gestureZoomScale = latestGestureScale
             }
     }
@@ -89,7 +91,7 @@ struct EmojiArtView: View {
         DragGesture()
             .onEnded { finalDragGestureValue in
                 steadyStatePanOffset = steadyStatePanOffset + finalDragGestureValue.translation / zoomScale
-            }.updating($gesturePanOffset) { latestGestureScale, gesturePanOffset, transition in
+            }.updating($gesturePanOffset) { latestGestureScale, gesturePanOffset, transaction in
                 gesturePanOffset = latestGestureScale.translation / zoomScale
             }
     }
